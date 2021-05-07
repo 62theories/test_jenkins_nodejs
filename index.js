7379f97b7ed75ff7ebcae04ec650c9b8fd5ed237
ball <nattawatx86@gmail.com>
2021-05-07 16:06:21 +0700

    test

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     return res.send(`My wealth and treasures? If you want it, I'll let you have it. Look for it, I left all of it at that place!`)
 })
 
+app.get('/test', (req, res) => {
+    return res.send(`test test`)
+})
+
 module.exports = app.listen(process.env.PORT || 5000, () => {
     console.log(`running on port ${process.env.PORT || 5000}`);
 })
